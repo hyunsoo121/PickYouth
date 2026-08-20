7f075f0f0697fafee8d537feb3f4878bfcc00b9b
ggom121@naver.com <ggom121@naver.com>
2026-08-20 14:09:36 +0900

    feat: queryDSL + swagger 설정

--- backend/src/main/java/com/Hyunsoo/PickYouth/global/config/SecurityConfig.java
+++ backend/src/main/java/com/Hyunsoo/PickYouth/global/config/SecurityConfig.java
@@ -55,6 +55,8 @@ public class SecurityConfig {
                     .permitAll()
                     .requestMatchers(HttpMethod.GET, "/api/subsidies/**")
                     .permitAll()
+                    .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
+                    .permitAll()
                     .anyRequest()
                     .authenticated())
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
